8493d8acaa5717e7790ec0f634ece6523defb504
Draichi <lucasdraichi@gmail.com>
2017-11-12 02:30:16 -0200

    CORS able

--- config/express.js
+++ config/express.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 //middlewares
+app.use(cors());
 app.use(bodyParser.json()); //is needed to use 'req.body' in job.js
 
 consign({ cwd: 'app' })
